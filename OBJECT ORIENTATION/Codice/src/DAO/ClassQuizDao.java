4519c71b6a44219e294c9a41e56713dd68f65b4d
NoemiSpera <96572076+NoemiSpera@users.noreply.github.com>
2022-02-18 21:54:57 +0100

    Create ClassQuizDao.java

---
+++ OBJECT ORIENTATION/Codice/src/DAO/ClassQuizDao.java
@@ -0,0 +1,11 @@
+package DAO;
+
+public interface QuizSvoltiDAO {
+	
+	  //getter RispostaData
+		public String getRispostaData();
+		//getter and setter PunteggioQuiz
+		public float getPunteggioQuiz();
+		public void setPunteggioQuiz(float punteggioQuiz);
+
+}
