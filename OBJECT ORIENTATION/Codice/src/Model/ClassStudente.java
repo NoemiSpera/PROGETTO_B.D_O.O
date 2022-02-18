4683838a1432d957a515ee5744ff98fc81157728
NoemiSpera <96572076+NoemiSpera@users.noreply.github.com>
2022-02-18 20:37:31 +0100

    Create ClassStudente.java

---
+++ OBJECT ORIENTATION/Codice/src/Model/ClassStudente.java
@@ -0,0 +1,26 @@
+package Classes;
+
+import java.util.ArrayList;
+
+public class Studente extends Utente {
+
+	private String IdStudente;
+	
+	public ArrayList <Corso> CorsiSeguiti=new ArrayList <Corso>();
+	public ArrayList<QuizSvolti> TestSvolto=new ArrayList<QuizSvolti>();
+	
+	
+	public Studente(String nome, String cognome, String login, String password) {
+		super(nome, cognome, login, password);
+		// TODO Auto-generated constructor stub
+	
+	}
+
+	//METODI
+	
+	   //getter id_studente
+	public String getIdStudente() {
+		return IdStudente;
+	}
+	
+}
