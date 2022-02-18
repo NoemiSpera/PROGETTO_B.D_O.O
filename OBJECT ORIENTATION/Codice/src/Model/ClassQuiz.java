d195ac6567effe2d2c457cbb43e2b85da781973a
NoemiSpera <96572076+NoemiSpera@users.noreply.github.com>
2022-02-18 20:41:07 +0100

    Create ClassQuiz.java

---
+++ OBJECT ORIENTATION/Codice/src/Model/ClassQuiz.java
@@ -0,0 +1,33 @@
+package Classes;
+
+public class Quiz {
+	
+	
+	protected String Domanda;
+	protected String IdQuiz;
+
+
+  public Test Composto;
+	
+	
+	public Quiz(String domanda2, String idQuiz2) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	//METODI 
+	
+	
+	    //getter IdQuiz
+	 public String  getIdQuiz() {
+		 return IdQuiz;
+	 }
+	
+
+	   //getter Domanda
+	 public String getDomanda() {
+		 return Domanda;
+	 }
+	
+}
