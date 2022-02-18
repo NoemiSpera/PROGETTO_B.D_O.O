f1cc5f87e2ee8deb6af02efc53aeea1456d8f21c
NoemiSpera <96572076+NoemiSpera@users.noreply.github.com>
2022-02-18 21:53:20 +0100

    Create CorsoDAO.java

---
+++ OBJECT ORIENTATION/Codice/src/DAO/CorsoDAO.java
@@ -0,0 +1,21 @@
+package DAO;
+
+import java.sql.Date;
+
+public interface CorsoDAO {
+
+	
+	
+	//getter and setter NomeCorso
+	public String getNomeCorso();
+	public void setNomeCorso(String nomeCorso);
+    //getter DataInizio
+	public Date getDataInizio();
+	//public void setDataInzio(String dataInizio);
+	//getter DataFine
+	public Date getDataFine();
+	//public void setDataFine(String dataFine);
+	
+		
+		
+}
