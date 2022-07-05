a4b2544d1e0d1c2cbba3b5d16f7e973deb028b4d
NoemiSpera <96572076+NoemiSpera@users.noreply.github.com>
2022-07-05 14:11:40 +0200

    Update Corso.java

--- OBJECT ORIENTATION/src/Model/Corso.java
+++ OBJECT ORIENTATION/src/Model/Corso.java
@@ -1,22 +1,22 @@
-package Model;
+package model;
 
 import java.sql.Date;
 
 public class Corso {
-
-	private String nome;
-	private String data_inizio;
-	private String data_fine;
 	private String cod_corso;
+	private String nome;
+	private Date data_inizio;
+	private Date data_fine;
 	private String id_ins;
 	
 
- public Corso (String cod_corso, String nome, String id_ins, String data_inizio, String data_fine) {
-		this.setCod_corso(cod_corso);
-		this.setNome(nome);
-		this.setId_ins(id_ins);
-		this.setData_inizio(data_inizio);
-		this.setData_fine(data_fine);
+ public Corso (String cod_corso, String nome, String id_ins, Date d_ini, Date d_fin) {
+		this.cod_corso=cod_corso;;
+		this.nome=nome;
+		this.id_ins=id_ins;
+		this.data_inizio=d_ini;
+		this.data_fine=d_fin;
+		
 	}
 
 
@@ -30,25 +30,24 @@ public void setId_ins(String id_ins) {
 }
 
 
-public String getData_fine() {
-	return data_fine;
-}
-
-
-public void setData_fine(String data_fine) {
-	this.data_fine = data_fine;
-}
-
-
-public String getData_inizio() {
+public Date getD_ini() {
 	return data_inizio;
 }
 
 
-public void setData_inizio(String data_inizio) {
-	this.data_inizio = data_inizio;
+public void setD_ini(Date d_ini ) {
+	this.data_inizio = d_ini;
 }
 
+
+public Date getD_fin() {
+	return data_fine;
+}
+
+
+public void setD_fin(Date d_fin ) {
+	this.data_fine = d_fin;
+}
 
 public String getNome() {
 	return nome;
