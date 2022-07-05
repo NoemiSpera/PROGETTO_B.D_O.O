677de6163a08fce45f927c13b519cadd9db098be
NoemiSpera <96572076+NoemiSpera@users.noreply.github.com>
2022-07-05 13:34:12 +0200

    Update Home.java

--- OBJECT ORIENTATION/src/GUI/Home.java
+++ OBJECT ORIENTATION/src/GUI/Home.java
@@ -1,26 +1,39 @@
 package GUI;
-import Controlller.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
+
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
+
+import controller.*;
+
 import java.awt.Color;
 import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.Canvas;
 import javax.swing.JEditorPane;
+import java.awt.Toolkit;
 
 public class Home extends JFrame {
-	public Home(controller contr) {
-		controller c=contr;
+	
+	ImageIcon logo= new ImageIcon(getClass().getClassLoader().getResource(("Custom-Icon-Design-Flatastic-2-Faq.ico")));
+	
+	public Home(Controller contr) {
+		Controller c=contr;
 		JPanel panel = new JPanel();
+		panel.setBackground(new Color(204, 102, 102));
+		panel.setForeground(new Color(204, 102, 102));
 		getContentPane().add(panel, BorderLayout.CENTER);
 		panel.setLayout(null);
 		
 		JButton btnSignIn = new JButton("Sign In");
+		btnSignIn.setFont(new Font("Cambria Math", Font.PLAIN, 13));
+		btnSignIn.setForeground(new Color(0, 0, 0));
+		btnSignIn.setBackground(new Color(255, 255, 255));
 		btnSignIn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				SignIn si= new SignIn(c);
@@ -35,6 +48,9 @@ public class Home extends JFrame {
 		panel.add(btnSignIn);
 		
 		JButton btnSignUp = new JButton("Sign Up");
+		btnSignUp.setFont(new Font("Cambria Math", Font.PLAIN, 13));
+		btnSignUp.setBackground(new Color(255, 255, 255));
+		btnSignUp.setForeground(new Color(0, 0, 0));
 		btnSignUp.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				SignUp sup= new SignUp(c); 
@@ -50,7 +66,8 @@ public class Home extends JFrame {
 		panel.add(btnSignUp);
 		
 		JLabel lblTitolo = new JLabel(" Home");
-		lblTitolo.setFont(new Font("Times New Roman", Font.PLAIN, 26));
+		lblTitolo.setForeground(new Color(0, 0, 0));
+		lblTitolo.setFont(new Font("Cambria Math", Font.PLAIN, 26));
 		lblTitolo.setBounds(0, 0, 89, 46);
 		panel.add(lblTitolo);
 
