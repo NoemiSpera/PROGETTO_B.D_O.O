ad4f7f3e8ad8a0ab2cea1d82227bc2c932e8352c
NoemiSpera <96572076+NoemiSpera@users.noreply.github.com>
2022-07-05 13:35:31 +0200

    Update SignIn.java

--- OBJECT ORIENTATION/src/GUI/SignIn.java
+++ OBJECT ORIENTATION/src/GUI/SignIn.java
@@ -1,9 +1,13 @@
 package GUI;
 
 import javax.swing.JFrame;
-import Model.*;
-import DAO.*;
-import Controlller.*;
+
+import controller.*;
+import dao.*;
+import implementazioneDao.ImpInsegnanteDao;
+import implementazioneDao.ImpStudenteDao;
+import model.*;
+
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import javax.swing.JButton;
@@ -16,7 +20,7 @@ import javax.swing.JPasswordField;
 import java.awt.TextField;
 import java.awt.Font;
 import javax.swing.JLabel;
-import com.jgoodies.forms.factories.DefaultComponentFactory;
+//import com.jgoodies.forms.factories.DefaultComponentFactory;
 import javax.swing.JCheckBox;
 import java.awt.Checkbox;
 import java.awt.Choice;
@@ -29,51 +33,51 @@ import javax.swing.ListSelectionModel;
 public class SignIn extends JFrame {
 	private JTextField textFieldLogin;
 	private JPasswordField passwordField;
-	public SignIn(controller contr) {
-		controller c=contr;
+	public SignIn(Controller contr) {
+		getContentPane().setBackground(new Color(204, 102, 102));
+		Controller c=contr;
 		getContentPane().setLayout(null);
 		
 		JLabel lblLogin = new JLabel("Login");
-		lblLogin.setFont(new Font("Times New Roman", Font.PLAIN, 17));
+		lblLogin.setFont(new Font("Cambria Math", Font.PLAIN, 17));
 		lblLogin.setBounds(63, 56, 83, 20);
 		getContentPane().add(lblLogin);
 		
 		JLabel lblPassword = new JLabel("Password");
-		lblPassword.setFont(new Font("Times New Roman", Font.PLAIN, 17));
+		lblPassword.setFont(new Font("Cambria Math", Font.PLAIN, 17));
 		lblPassword.setBounds(63, 109, 101, 22);
 		getContentPane().add(lblPassword);
 		
 		textFieldLogin = new JTextField();
-		textFieldLogin.setFont(new Font("Times New Roman", Font.PLAIN, 17));
+		textFieldLogin.setFont(new Font("Cambria Math", Font.PLAIN, 17));
 		textFieldLogin.setBounds(174, 56, 207, 20);
 		getContentPane().add(textFieldLogin);
 		textFieldLogin.setColumns(10);
 		
 		passwordField = new JPasswordField();
-		passwordField.setFont(new Font("Times New Roman", Font.PLAIN, 17));
+		passwordField.setFont(new Font("Cambria Math", Font.PLAIN, 17));
 		passwordField.setBounds(174, 109, 207, 20);
 		getContentPane().add(passwordField);
 		
 		JButton btnLogin = new JButton("Login");
+		btnLogin.setBackground(new Color(255, 255, 255));
 		btnLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 
 				String pas= new String (passwordField.getText());
-				StudenteDAO stu= new StudenteDAO();
-				InsegnanteDAO ins= new InsegnanteDAO();
-				String pas1=new String(stu.contrLog(textFieldLogin.getText(), "no"));
-				String pas2=new String(ins.contrLog(textFieldLogin.getText(), "no"));
+				String pas1=new String(c.contrLog(textFieldLogin.getText(), "no"));
+				String pas2=new String(c.contrLogI(textFieldLogin.getText(), "no"));
 				if(pas1.equals(pas)) {
 					SignInS sis=new SignInS(c, textFieldLogin.getText());
 					sis.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-					sis.setBounds(100, 100, 450, 300);
+					sis.setBounds(100, 100, 900, 500);
 					sis.show();
 					dispose();
 				}
 				else if (pas2.equals(pas)) {
 				SignInI sii=new SignInI(c,textFieldLogin.getText());
 				sii.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				sii.setBounds(100, 100, 450, 300);
+				sii.setBounds(100, 100, 600, 500);
 				sii.show();
 				dispose();
 
@@ -85,11 +89,12 @@ public class SignIn extends JFrame {
 });	
 
 		btnLogin.setToolTipText("Procedi");
-		btnLogin.setFont(new Font("Times New Roman", Font.PLAIN, 17));
-		btnLogin.setBounds(207, 214, 89, 23);
+		btnLogin.setFont(new Font("Cambria Math", Font.PLAIN, 17));
+		btnLogin.setBounds(195, 214, 101, 23);
 		getContentPane().add(btnLogin);
 		
 		JButton btnAnnulla = new JButton("Annulla");
+		btnAnnulla.setBackground(new Color(255, 255, 255));
 		btnAnnulla.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Home ho= new Home(c); 
@@ -100,8 +105,8 @@ public class SignIn extends JFrame {
 			}
 		});
 		btnAnnulla.setToolTipText("Torni a home\r\n");
-		btnAnnulla.setFont(new Font("Times New Roman", Font.PLAIN, 17));
-		btnAnnulla.setBounds(319, 214, 89, 23);
+		btnAnnulla.setFont(new Font("Cambria Math", Font.PLAIN, 17));
+		btnAnnulla.setBounds(319, 214, 107, 23);
 		getContentPane().add(btnAnnulla);
 
 
