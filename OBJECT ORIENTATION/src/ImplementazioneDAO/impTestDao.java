efd5bcd4931cb8316c942883b7ff8fdb5c387c33
NoemiSpera <96572076+NoemiSpera@users.noreply.github.com>
2022-07-05 14:11:02 +0200

    Update impTestDao.java

--- OBJECT ORIENTATION/src/ImplementazioneDAO/impTestDao.java
+++ OBJECT ORIENTATION/src/ImplementazioneDAO/impTestDao.java
@@ -1,21 +1,50 @@
 package implementazioneDao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-import database_connection.database_connection;
+import database_connection.Database_connection;
+import model.Test;
 
-public class impTestDao {
+public class ImpTestDao {
 	public Connection connessione;
 	
-	public void TestDAO() {
+	public void impTestDao() {
         try {
-            connessione = database_connection.getInstance().getConnection();
+            connessione = Database_connection.getInstance().getConnection();
         } catch (SQLException e) {
             System.out.println("Connessione fallita.");
             e.printStackTrace();
         }
     }
-	
 
+	public void InserisciTest(Test t) {
+		
+	    try {
+	        connessione = Database_connection.getInstance().getConnection();
+	    } catch (SQLException e) {
+	        System.out.println("Connessione fallita.");
+	        e.printStackTrace();
+	    }
+		
+		PreparedStatement insert;
+	    try {
+	        insert = connessione.prepareStatement(
+	                "INSERT INTO test VALUES ('"+t.getNome_id()+"','"+t.getN_quiz()+"','"+t.getData()+"','"+t.getOra_inizio()+"','"+t.getOra_fine()+"','"+t.getCod_corso()+"','"+t.getId_ins()+"');");
+
+	    insert.executeUpdate();
+
+	    } 
+	    catch (SQLException e) {
+
+	        e.printStackTrace();
+
+	    }
+		
+	}
+	
+	/*public void riempiTable (String codTest, String [][] tab) {
+}*/
+	
 }
