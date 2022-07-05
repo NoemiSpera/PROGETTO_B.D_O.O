3af21df936eb6017176d19fe8666c09f757e3d1b
NoemiSpera <96572076+NoemiSpera@users.noreply.github.com>
2022-07-05 14:10:14 +0200

    Update impStudenteDao.java

--- OBJECT ORIENTATION/src/ImplementazioneDAO/impStudenteDao.java
+++ OBJECT ORIENTATION/src/ImplementazioneDAO/impStudenteDao.java
@@ -5,16 +5,16 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import Model.Studente;
-import database_connection.database_connection;
+import database_connection.Database_connection;
+import model.Studente;
 
 
-public class impStudenteDao {
+public class ImpStudenteDao {
 	public Connection connessione;
 	
-	public impStudenteDao() {
+	public ImpStudenteDao() {
         try {
-            connessione = database_connection.getInstance().getConnection();
+            connessione = Database_connection.getInstance().getConnection();
         } catch (SQLException e) {
             System.out.println("Connessione fallita.");
             e.printStackTrace();
@@ -22,9 +22,28 @@ public class impStudenteDao {
     }
 
 	public void InserisciStud(Studente s) {
-		// TODO Auto-generated method stub
 		
-	}
+	    try {
+	        connessione = Database_connection.getInstance().getConnection();
+	    } catch (SQLException e) {
+	        System.out.println("Connessione fallita.");
+	        e.printStackTrace();
+	    }
+
+			PreparedStatement insert;
+	        try {
+	            insert = connessione.prepareStatement(
+	                    "INSERT INTO STUDENTE VALUES ('"+s.getMatricola_Stud()+"','"+s.getNome()+"','"+s.getCognome()+"','"+s.getLogin()+"','"+s.getPassword()+"');");
+
+	        insert.execute();
+
+	        } 
+	        catch (SQLException e) {
+
+	            e.printStackTrace();
+
+	        }
+		}
 	
 	
 }
