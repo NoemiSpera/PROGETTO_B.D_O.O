151ac6aafb4f4bd4e8e7f8d976bb2cc7f83f5ff1
Marziap <94360287+Marziap@users.noreply.github.com>
2022-07-10 12:25:08 +0200

    Add files via upload

---
+++ OBJECT ORIENTATION/IMPLEMENTAZIONE/src/implementazioneDao/ImpQuiz_Risp_ApeDao.java
@@ -0,0 +1,56 @@
+package implementazioneDao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import database_connection.Database_connection;
+import model.Insegnante;
+import model.Quiz_risp_ape;
+
+public class ImpQuiz_Risp_ApeDao implements dao.Quiz_Risp_ApeDAO{
+
+	public Connection connessione;
+	
+	public void ImpQuiz_Risp_ApeDao (){
+        try {
+            connessione = Database_connection.getInstance().getConnection();
+        } catch (SQLException e) {
+            System.out.println("Connessione fallita.");
+            e.printStackTrace();
+        }
+        
+        
+        
+        
+    }
+
+	public void Quiz_Risp_Ape(Quiz_risp_ape qra) {
+		
+	    try {
+	        connessione = Database_connection.getInstance().getConnection();
+	    } catch (SQLException e) {
+	        System.out.println("Connessione fallita.");
+	        e.printStackTrace();
+	    }
+		
+
+	    try {
+	    	PreparedStatement MyStmt = connessione.prepareStatement("INSERT INTO QUIZ_RISP_APE VALUES (?,?,?,?,?,?)");
+	        MyStmt.setObject(1, qra.getId_quiz());
+	        MyStmt.setObject(2, qra.getDomanda());
+	        MyStmt.setInt(3, qra.getMax_lenght());
+	        MyStmt.setFloat(4, qra.getPunt_max());
+	        MyStmt.setFloat(5, qra.getPunt_min());
+	        MyStmt.setObject(6, qra.getId_test());
+	        MyStmt.executeUpdate();
+
+	    } 
+	    catch (SQLException e) {
+
+	        e.printStackTrace();
+
+	    }
+		
+	}
+}
---
+++ OBJECT ORIENTATION/IMPLEMENTAZIONE/src/implementazioneDao/ImpStudenteDao.java
@@ -0,0 +1,49 @@
+package implementazioneDao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import database_connection.Database_connection;
+import model.Studente;
+
+
+public class ImpStudenteDao implements dao.StudenteDAO{
+	public Connection connessione;
+	
+	public ImpStudenteDao() {
+        try {
+            connessione = Database_connection.getInstance().getConnection();
+        } catch (SQLException e) {
+            System.out.println("Connessione fallita.");
+            e.printStackTrace();
+        }
+    }
+
+	public void InserisciStud(Studente s) {
+		
+	    try {
+	        connessione = Database_connection.getInstance().getConnection();
+	    } catch (SQLException e) {
+	        System.out.println("Connessione fallita.");
+	        e.printStackTrace();
+	    }
+
+			PreparedStatement insert;
+	        try {
+	            insert = connessione.prepareStatement(
+	                    "INSERT INTO STUDENTE VALUES ('"+s.getMatricola_Stud()+"','"+s.getNome()+"','"+s.getCognome()+"','"+s.getLogin()+"','"+s.getPassword()+"');");
+
+	        insert.execute();
+
+	        } 
+	        catch (SQLException e) {
+
+	            e.printStackTrace();
+
+	        }
+		}
+	
+	
+}
