085b77895f599052e8f43e700b846aaa74b947ca
Marziap <94360287+Marziap@users.noreply.github.com>
2022-07-10 11:45:00 +0200

    Add files via upload

---
+++ OBJECT ORIENTATION/IMPLEMENTAZIONE/src/implementazioneDao/ImpCorsoDao.java
@@ -0,0 +1,62 @@
+package implementazioneDao;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+
+import database_connection.Database_connection;
+import model.Corso;
+
+public class ImpCorsoDao {
+
+	public Connection connessione;
+	
+	public void CorsoDAO() {
+        try {
+            connessione = Database_connection.getInstance().getConnection();
+        } catch (SQLException e) {
+            System.out.println("Connessione fallita.");
+            e.printStackTrace();
+        }
+    }
+
+	public void InserisciCorso(Corso c) {
+	    try {
+	        connessione = Database_connection.getInstance().getConnection();
+	    } catch (SQLException e) {
+	        System.out.println("Connessione fallita.");
+	        e.printStackTrace();
+	    }
+			
+	        try {
+
+	        	PreparedStatement MyStmt = connessione.prepareStatement("INSERT INTO CORSO VALUES (?,?,?,?,?)");
+	            MyStmt.setObject(1, c.getCod_corso());
+	            MyStmt.setObject(2, c.getNome());
+	            MyStmt.setObject(3, c.getId_ins());
+	            
+	            String pattern = "yyyy-MM-dd";
+	    		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+	    		
+	    		String date1 = simpleDateFormat.format(c.getD_ini());
+	    		Date d_ini1=Date.valueOf(date1);
+	    		
+	    		String date2 = simpleDateFormat.format(c.getD_fin());
+	    		Date d_fin1=Date.valueOf(date2);
+	    		
+	            MyStmt.setObject(4, d_ini1);
+	            MyStmt.setObject(5, d_fin1);
+	            MyStmt.executeUpdate();
+
+	        } 
+	        catch (SQLException e) {
+
+	            e.printStackTrace();
+
+	        }
+			
+		}
+	
+}
---
+++ OBJECT ORIENTATION/IMPLEMENTAZIONE/src/implementazioneDao/ImpQuiz_Risp_MulDao.java
@@ -0,0 +1,61 @@
+package implementazioneDao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import database_connection.Database_connection;
+import model.Insegnante;
+import model.Quiz_risp_mul;
+
+public class ImpQuiz_Risp_MulDao {
+
+	public Connection connessione;
+	
+	public void ImpQuiz_Risp_MulDao (){
+        try {
+            connessione = Database_connection.getInstance().getConnection();
+        } catch (SQLException e) {
+            System.out.println("Connessione fallita.");
+            e.printStackTrace();
+        }
+        
+        
+        
+        
+    }
+
+	public void Quiz_Risp_Multi(Quiz_risp_mul qrm) {
+		
+	    try {
+	        connessione = Database_connection.getInstance().getConnection();
+	    } catch (SQLException e) {
+	        System.out.println("Connessione fallita.");
+	        e.printStackTrace();
+	    }
+		
+
+	    try {
+	    	PreparedStatement MyStmt = connessione.prepareStatement("INSERT INTO QUIZ_RISP_MUL VALUES (?,?,?,?,?,?,?,?,?,?)");
+	        MyStmt.setObject(1, qrm.getId_quiz());
+	        MyStmt.setObject(2, qrm.getDomanda());
+	        MyStmt.setObject(3, qrm.getA());
+	        MyStmt.setObject(4, qrm.getB());
+	        MyStmt.setObject(5, qrm.getC());
+	        MyStmt.setObject(6, qrm.getD());
+	        MyStmt.setObject(7, qrm.getRispo_corr());
+	        MyStmt.setInt(8, qrm.getPunt_c());
+	        MyStmt.setInt(9, qrm.getPunt_e());
+	        MyStmt.setObject(10, qrm.getId_test());
+
+	        MyStmt.executeUpdate();
+
+	    } 
+	    catch (SQLException e) {
+
+	        e.printStackTrace();
+
+	    }
+		
+	}
+}
